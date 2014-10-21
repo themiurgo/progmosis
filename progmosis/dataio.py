d51bb2bb5fb8de79dae9fdbe6288ef07572e9f34
Antonio Lima <anto87@gmail.com>
2014-10-21 13:28:08 -0400

    Parse locations now return sorted history, tests added

--- progmosis/dataio.py
+++ progmosis/dataio.py
@@ -1,11 +1,30 @@
+import collections
 import csv
 import dateutil.parser
 
-def parse_location(text):
-    reader = csv.DictReader(text)
+def parse_location(input_fobj):
+    """Parse location and return a dict of time-ordered lists of tuples.
+
+    history[location] = [(time, infected_fraction), ...]
+
+    input_fobj -- file object
+
+    """
+
+    # Convert csv to history
+    reader = csv.DictReader(input_fobj)
+    history = collections.defaultdict(list)
     for row in reader:
-        dateutil.parser.parse(row['datetime'])
-    try:
-        assert 0 <= row['infected_fraction'] <= 1
-    except AssertionError:
-        raise ValueError("Fraction must be between 0 and 1")
+        row['infected_fraction'] = float(row['infected_fraction'])
+        try:
+            assert 0 <= row['infected_fraction'] <= 1
+        except AssertionError:
+            raise ValueError("Fraction must be between 0 and 1")
+        dt = dateutil.parser.parse(row['datetime'])
+        history[row['location']].append((dt, float(row['infected_fraction'])))
+
+    # Sort history
+    for location, location_history in history.iteritems():
+        history[location] = sorted(location_history, key=lambda x: x[0])
+        
+    return history
--- progmosis/tests.py
+++ progmosis/tests.py
@@ -1,32 +1,54 @@
+import datetime
+import io
 import unittest
 
 import dataio
-import io
 
 class LocationsTestCase(unittest.TestCase):
     # Location file
 
-    locations_bad_fraction = io.StringIO(u"""datetime,location,infected_fraction
-    2014-01-01,Italy,0
-    2014-01-01,France,0
-    2014-01-10,Italy,10
-    2014-01-12,France,5""")
-
-    locations_good_fraction = io.StringIO(u"""datetime,location,infected_fraction
-    2014-01-01,Italy,0
-    2014-01-01,France,0
-    2014-01-10,Italy,0.01
-    2014-01-12,France,0.005""")
-
-    locations_bad_date = io.StringIO(u"""datetime,location,infected_fraction
-    2014-01-01,Italy,0
-    2014-13-01,France,0
-    2014-01-10,Italy,0.01
-    2014-01-12,France,0.005""")
-
     def test_locations(self):
-        self.assertRaises(ValueError, dataio.parse_location, self.locations_bad_date)
-        self.assertRaises(ValueError, dataio.parse_location, self.locations_bad_fraction)
+        locations_bad_fraction = io.StringIO(u"""datetime,location,infected_fraction
+        2014-01-01,Italy,0
+        2014-01-01,France,0
+        2014-01-10,Italy,10
+        2014-01-12,France,5""")
+
+        locations_bad_date = io.StringIO(u"""datetime,location,infected_fraction
+        2014-01-01,Italy,0
+        2014-13-01,France,0
+        2014-01-10,Italy,0.01
+        2014-01-12,France,0.005""")
+        self.assertRaises(ValueError, dataio.parse_location, locations_bad_date)
+        self.assertRaises(ValueError, dataio.parse_location, locations_bad_fraction)
+
+    def test_simple(self):
+        simple = io.StringIO(u"""datetime,location,infected_fraction
+        2014-01-01,Italy,0
+        2014-01-01,France,0
+        2014-01-10,Italy,0.01
+        2014-01-12,France,0.005""")
+
+        simple_output = {
+            "Italy": [(datetime.datetime(2014, 01, 01), 0), (datetime.datetime(2014, 01, 10), 0.01)],
+            "France": [(datetime.datetime(2014, 01, 01), 0), (datetime.datetime(2014, 01, 12), 0.005)]
+        }
+
+        self.assertEqual(dataio.parse_location(simple), simple_output)
+
+    def test_sorting(self):
+        simple = io.StringIO(u"""datetime,location,infected_fraction
+        2014-01-10,Italy,0.01
+        2014-01-01,France,0
+        2014-01-01,Italy,0
+        2014-01-12,France,0.005""")
+
+        simple_output = {
+            "Italy": [(datetime.datetime(2014, 01, 01), 0), (datetime.datetime(2014, 01, 10), 0.01)],
+            "France": [(datetime.datetime(2014, 01, 01), 0), (datetime.datetime(2014, 01, 12), 0.005)]
+        }
+
+        self.assertEqual(dataio.parse_location(simple), simple_output)
 
 if __name__ == '__main__':
     unittest.main()
